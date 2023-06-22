87cef8177a3866e0423ea4590fa24784e163db4b
kyushio <fabiodosreisf@gmail.com>
2023-06-21 21:01:15 -0300

    adicionada pagina Punch

---
+++ Pages/Punch.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kuku.Pages
+{
+    public class PunchModel : PageModel
+    {
+        private readonly ILogger<PunchModel> _logger;
+
+        public PunchModel(ILogger<PunchModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
