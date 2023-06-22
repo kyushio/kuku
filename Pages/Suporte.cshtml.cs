5d432a95e1cca80519137e8b429723ad44c3036e
kyushio <fabiodosreisf@gmail.com>
2023-06-21 22:20:58 -0300

    Acrescentada Pagina de Suporte

---
+++ Pages/Suporte.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kuku.Pages
+{
+    public class SuporteModel : PageModel
+    {
+        private readonly ILogger<SuporteModel> _logger;
+
+        public SuporteModel(ILogger<SuporteModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
