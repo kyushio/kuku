05cb739b3e4eb7f029bafad5a17103086856330e
kyushio <fabiodosreisf@gmail.com>
2023-06-23 14:49:37 -0300

    New Page Gilberto

---
+++ Pages/Gilberto.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kuku.Pages
+{
+    public class GilbertoModel : PageModel
+    {
+        private readonly ILogger<GilbertoModel> _logger;
+
+        public GilbertoModel(ILogger<GilbertoModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
