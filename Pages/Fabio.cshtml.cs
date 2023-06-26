6cf9a9a3e0c796666231f18f9345d6d3b87e1d1b
kyushio <fabiodosreisf@gmail.com>
2023-06-26 16:46:22 -0300

    Adicionado Pagina Fabio

---
+++ Pages/Fabio.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kuku.Pages
+{
+    public class FabioModel : PageModel
+    {
+        private readonly ILogger<FabioModel> _logger;
+
+        public FabioModel(ILogger<FabioModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
