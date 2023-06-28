f62a26325190ffe06d3b008511555b66efd4c97e
kyushio <fabiodosreisf@gmail.com>
2023-06-27 22:50:26 -0300

    Criado Pagina Bruno

---
+++ Pages/Bruno.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kuku.Pages
+{
+    public class BrunoModel : PageModel
+    {
+        private readonly ILogger<BrunoModel> _logger;
+
+        public BrunoModel(ILogger<BrunoModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
